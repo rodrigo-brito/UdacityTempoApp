445b4966ee79122d79a6de2d8d00969519ccf8ad
Rodrigo Brito <rig.10@hotmail.com>
2016-08-24 21:44:34 -0300

    Inclusão das Activities de configuração

--- app/src/main/java/net/rodrigobrito/tempo/DetailActivity.java
+++ app/src/main/java/net/rodrigobrito/tempo/DetailActivity.java
@@ -1,12 +1,21 @@
 package net.rodrigobrito.tempo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            TextView textView = (TextView) findViewById(R.id.previsao);
+            textView.setText(forecastStr);
+        }
     }
 }
